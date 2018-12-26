3d42d179a0914317426a290bc0f72da9145b3cc2
Parteek S <parteek111269cse@gmail.com>
2018-12-26 15:16:27 +0530

    splashscreen added

---
+++ src/@drago/splashscreen.service.ts
@@ -0,0 +1,59 @@
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { animate, AnimationBuilder, AnimationPlayer, style } from '@angular/animations';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, take } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class dragoSplashScreenService{
+	public splashScreenEl: any;
+	public player: AnimationPlayer;
+	constructor(private _animationBuilder: AnimationBuilder, @Inject(DOCUMENT) private _document: any, private _router: Router){
+		// Initialize
+        this.init();
+	}
+	private init(): void{
+		// Get the splash screen element
+        this.splashScreenEl = this._document.body.querySelector('#drago-splash-screen');
+        if (this.splashScreenEl){
+        	// Hide it on the first NavigationEnd event
+        	this._router.events.pipe(
+        		filter((event => event instanceof NavigationEnd)),
+                take(1)
+            ).subscribe(() => {
+                setTimeout(() => {
+                    this.hide();
+                });
+            });
+        }
+	}
+	// show splash screen
+	show(): void{
+        this.player = this._animationBuilder.build([
+            style({
+                opacity: '0',
+                zIndex : '99999'
+            }),
+            animate('400ms ease', style({opacity: '1'}))
+        ]).create(this.splashScreenEl);
+        setTimeout(() => {
+            this.player.play();
+        }, 0);
+    }
+    // hide splash screen
+    hide(): void{
+        this.player = this._animationBuilder.build([
+            style({opacity: '1'}),
+            animate('400ms ease', style({
+                opacity: '0',
+                zIndex : '-10'
+            }))
+        ]).create(this.splashScreenEl);
+		setTimeout(() => {
+            this.player.play();
+        }, 1000);
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { dragoSplashScreenService } from '../@drago/splashscreen.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	constructor(private dragoSplashScreenService: dragoSplashScreenService){
+
+	}
 }
